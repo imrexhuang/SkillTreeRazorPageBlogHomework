944964259006c32a453bd0877a3dd9ea5e797850
Rex <im@rex.tw>
2020-05-19 21:57:12 +0800

    部落格文章資料由html改成從資料庫撈資料

--- Data/RazorpageblogContext.cs
+++ Data/RazorpageblogContext.cs
@@ -3,7 +3,6 @@ using SkillTreeRazorPageBlogSample.Models;
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
-using SkillTreeRazorPageBlogSample.Models;
 
 namespace SkillTreeRazorPageBlogSample.Data
 {
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -5,21 +5,27 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using SkillTreeRazorPageBlogSample.Models;
+using SkillTreeRazorPageBlogSample.Data;
 
 namespace SkillTreeRazorPageBlogSample.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly RazorpageblogContext _context;
         private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(RazorpageblogContext context, ILogger<IndexModel> logger)
         {
+            _context = context;
             _logger = logger;
         }
 
+        public IList<Article> Article { get; set; }
+
         public void OnGet()
         {
-
+            Article =  _context.Articles.ToList();
         }
     }
 }
