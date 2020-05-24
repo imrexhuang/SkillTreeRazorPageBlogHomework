307f945f413745eef1f4ed52036530a8c638e147
Rex <im@rex.tw>
2020-05-24 13:22:12 +0800

    改用自訂的IndexViewClass

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -21,11 +21,34 @@ namespace SkillTreeRazorPageBlogSample.Pages
             _logger = logger;
         }
 
-        public IList<Article> Article { get; set; }
+        public IEnumerable<IndexViewClass> Article { get; set; }
 
         public void OnGet()
         {
-            Article =  _context.Articles.ToList();
+            Article = _context.Articles
+                .Select(
+                    d => new IndexViewClass
+                    {
+                        Id = d.Id,
+                        Title = d.Title,
+                        Body = d.Body,
+                        CoverPhoto = d.CoverPhoto,
+                        CreateDate = d.CreateDate,
+                        Tags = d.Tags
+                    })
+                    .ToList();
         }
+
     }
+
+    public class IndexViewClass
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public string CoverPhoto { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string Tags { get; set; }
+    }
+
 }
