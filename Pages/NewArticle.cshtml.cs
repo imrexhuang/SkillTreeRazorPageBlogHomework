f968fb5d5dfd6f56cd4f52f4690ab1ec2bb40d08
Rex <im@rex.tw>
2020-05-24 20:05:20 +0800

    加入"新增文章"功能

---
+++ Pages/NewArticle.cshtml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using SkillTreeRazorPageBlogSample.Data;
+using SkillTreeRazorPageBlogSample.Models;
+
+namespace SkillTreeRazorPageBlogSample.Pages
+{
+    public class NewArticleModel : PageModel
+    {
+        private readonly SkillTreeRazorPageBlogSample.Data.RazorpageblogContext _context;
+
+        public NewArticleModel(SkillTreeRazorPageBlogSample.Data.RazorpageblogContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Article Article { get; set; }
+
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            //Scaffold之後，這行要自己加
+            Article.Id = Guid.NewGuid();
+            Article.CreateDate = DateTime.Now;
+
+            _context.Articles.Add(Article);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
