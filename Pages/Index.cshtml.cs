ee02ef726ea034d4a4aa56c31339049305d7a775
Rex <im@rex.tw>
2020-05-31 21:00:30 +0800

    增加搜尋功能:依關鍵字搜尋標題和內容

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -24,11 +24,21 @@ namespace SkillTreeRazorPageBlogSample.Pages
 
         public IEnumerable<IndexViewClass> Article { get; set; }
 
-        public void OnGet(int? p)
+        public void OnGet(int? p, string q)
         {
             int pageIndex = p.HasValue ? p.Value < 1 ? 1 : p.Value : 1;
             int articlePerPage = 5; //一頁顯示幾筆文章
-            Article = _context.Articles
+            var query = _context.Articles.AsQueryable();
+            var queryTitle = _context.Articles.AsQueryable();
+            var queryBody = _context.Articles.AsQueryable();
+            if (string.IsNullOrWhiteSpace(q) == false)
+            {
+                queryTitle = query.Where(d => d.Title.Contains(q));
+                queryBody = query.Where(d => d.Body.Contains(q));
+                query = queryTitle.Union(queryBody);
+            }
+
+            Article = query
                 .Select(
                     d => new IndexViewClass
                     {
