d36fad16e44beead29825b4521b531c4385499ee
Rex <im@rex.tw>
2020-06-01 23:38:50 +0800

    TODO: 將搜尋結果關鍵字套css高亮度顯示，還是尚未完成功能，POC驗證成功 參考資料 https://whosnailaspnetcoredocs.readthedocs.io/ko/latest/mvc/views/tag-helpers/authoring.html#a-working-email-tag-helper

--- Helper/Tag/SearchresulthighlightTagHelper.cs
+++ Helper/Tag/SearchresulthighlightTagHelper.cs
@@ -8,28 +8,31 @@ using Microsoft.AspNetCore.Mvc.Routing;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 
 // 參考 https://docs.microsoft.com/zh-tw/aspnet/core/mvc/views/tag-helpers/authoring?view=aspnetcore-3.1
+// https://whosnailaspnetcoredocs.readthedocs.io/ko/latest/mvc/views/tag-helpers/authoring.html#a-working-email-tag-helper
 
 namespace SkillTreeRazorPageBlogSample.Helper.Tag
 {
     public class SearchresulthighlightTagHelper : TagHelper
     {
-        public String searchString { get; set; }
-        public String articleBody { get; set; }
+        public string SearchString { get; set; }
+        public string ArticleBody { get; set; }
 
         public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
         {
             // TODO: 將搜尋結果關鍵字套css高亮度顯示，尚未完成功能
             output.TagName = "p";
             var content = await output.GetChildContentAsync();
-            if (string.IsNullOrWhiteSpace(content.GetContent()) == false)
-            {
+            //if (string.IsNullOrWhiteSpace(content.GetContent()) == false)
+            //{
                 var sb = new StringBuilder();
 
-                sb.Append(articleBody);
+                sb.Append("SearchresulthighlightTagHelper測試" + ArticleBody);
 
 
                 output.Content.SetHtmlContent(sb.ToString());
-            }
+
+                //output.TagMode = TagMode.StartTagAndEndTag;
+            //}
         }
     }
 }
